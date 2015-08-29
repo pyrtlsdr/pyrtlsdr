01115b431fc253b35399ea13b96ced12a9a13af2
nocarryr <matt@nomadic-recording.com>
2015-08-29 14:49:09 -0500

    Clean up unused code and fix pep8 styles

--- rtlsdr/rtlsdrtcp.py
+++ rtlsdr/rtlsdrtcp.py
@@ -26,15 +26,18 @@ except ImportError:
 
 MAX_BUFFER_SIZE = 4096
 
+
 class CommunicationError(Exception):
     def __init__(self, msg, source_exc=None):
         self.msg = msg
         self.source_exc = source_exc
+
     def __str__(self):
         s = self.msg
         if self.source_exc is not None:
             s = 'SOURCE EXCEPTION:\n%s\n\n%s' % (traceback.format_exc(), s)
         return s
+
 
 class RtlSdrTcpBase(RtlSdr):
     # Use port 1235 as default since rtl_tcp uses 1234
@@ -52,18 +55,23 @@ class RtlSdrTcpBase(RtlSdr):
         self.server_thread = None
         super(RtlSdrTcpBase, self).__init__(device_index, test_mode_enabled)
 
+
 class RtlSdrTcpServer(RtlSdrTcpBase):
-    '''Server that connects to a physical dongle to allow client connections
-    '''
+
+    """Server that connects to a physical dongle to allow client connections.
+    """
+
     def open(self, device_index=0, test_mode_enabled=False):
         if not self.device_ready:
             return
         super(RtlSdrTcpServer, self).open(device_index, test_mode_enabled)
+
     def run(self):
-        '''Runs the server thread and returns.  Use this only if you are
+        """Runs the server thread and returns.  Use this only if you are
         running mainline code afterwards.
         The server must explicitly be stopped by the stop method before exit.
-        '''
+
+        """
         if self.server_thread is None:
             self.server_thread = ServerThread(self)
         if self.server_thread.running.is_set():
@@ -73,10 +81,11 @@ class RtlSdrTcpServer(RtlSdrTcpBase):
         if self.server_thread.stopped.is_set():
             self.server_thread = None
             self.close()
+
     def run_forever(self):
-        '''Runs the server and begins a mainloop.
+        """Runs the server and begins a mainloop.
         The loop will exit with Ctrl-C.
-        '''
+        """
         self.run()
         while True:
             try:
@@ -84,15 +93,17 @@ class RtlSdrTcpServer(RtlSdrTcpBase):
             except KeyboardInterrupt:
                 self.close()
                 break
+
     def close(self):
-        '''Stops the server (if it's running) and closes the connection to the
+        """Stops the server (if it's running) and closes the connection to the
         dongle.
-        '''
+        """
         if self.server_thread is not None:
             if self.server_thread.running.is_set():
                 self.server_thread.stop()
             self.server_thread = None
         super(RtlSdrTcpServer, self).close()
+
 
 class ServerThread(threading.Thread):
     def __init__(self, rtl_sdr):
@@ -100,6 +111,7 @@ class ServerThread(threading.Thread):
         self.rtl_sdr = rtl_sdr
         self.running = threading.Event()
         self.stopped = threading.Event()
+
     def run(self):
         try:
             self.server = Server(self.rtl_sdr)
@@ -116,24 +128,26 @@ class ServerThread(threading.Thread):
         self.running.clear()
         rtl_sdr.device_ready = False
         self.stopped.set()
+
     def stop(self):
         self.server.shutdown()
         self.server.server_close()
         self.stopped.wait()
 
+
 class Server(TCPServer):
     REQUEST_RECV_SIZE = 1024
+
     def __init__(self, rtl_sdr):
         self.rtl_sdr = rtl_sdr
         server_addr = (rtl_sdr.hostname, rtl_sdr.port)
         TCPServer.__init__(self, server_addr, RequestHandler)
         self.handlers = set()
+
     def server_close(self):
         for h in self.handlers:
             h.close()
-    #def finish_request(self, request, client_address):
-    #    handler = TcpServer.finish_request(self, request, client_address)
-    #    self.handlers.add(handler)
+
 
 API_METHODS = (
     'get_center_freq', 'set_center_freq',
@@ -152,43 +166,48 @@ API_DESCRIPTORS = {
     'freq_correction',
 }
 
+
 class MessageBase(object):
-    '''Base class for messages sent between clients and servers.
+
+    """Base class for messages sent between clients and servers.
+
     Hanldes serialization/deserialization and communication with
     socket type objects.
-    '''
+
+    """
+
     def __init__(self, **kwargs):
         self.data_len = None
         self.timestamp = kwargs.get('timestamp')
         self.header = self.get_header(**kwargs)
         self.data = self.get_data(**kwargs)
         self.data_is_complex = False
+
     @staticmethod
     def _send(sock, data):
         r, w, e = select.select([], [sock], [], .5)
         if sock not in w:
             raise CommunicationError('socket %r not ready for write' % (sock))
         return sock.send(data)
+
     @staticmethod
     def _recv(sock):
         r, w, e = select.select([sock], [], [])
         if sock not in r:
             raise CommunicationError('socket %r not ready for read' % (sock))
         return sock.recv(MAX_BUFFER_SIZE)
+
     @classmethod
     def from_remote(cls, sock):
-        '''Reads data for the socket buffer and reconstructs the appropriate
+        """Reads data for the socket buffer and reconstructs the appropriate
         message that was sent by the other end.
-        '''
+        """
         header = cls._recv(sock)
-        try:
-            kwargs = numpyjson.loads(header)
-        except ValueError:
-            #print(header)
-            raise
+        kwargs = numpyjson.loads(header)
         if kwargs.get('ACK'):
             cls = AckMessage
         return cls(**kwargs)
+
     def get_header(self, **kwargs):
         d = {}
         ts = kwargs.get('timestamp')
@@ -196,16 +215,21 @@ class MessageBase(object):
             ts = time.time()
         d['timestamp'] = ts
         return d
+
     def get_data(self, **kwargs):
         return kwargs.get('data')
+
     def send_message(self, sock):
         header, data = self._serialize()
         self._send(sock, header)
+
     def get_response(self, sock):
         cls = self.get_response_class()
         return cls.from_remote(sock)
+
     def get_ack_response(self, sock):
         return AckMessage.from_remote(sock)
+
     def _serialize(self):
         if not self.data_is_complex:
             d = self.header.copy()
@@ -216,20 +240,26 @@ class MessageBase(object):
             self.data_len = len(data)
         header = self._serialize_header()
         return header, data
+
     def _serialize_header(self):
         header = self.header
         header['data_len'] = self.data_len
         return numpyjson.dumps(self.header)
+
     def _serialize_data(self):
         return numpyjson.dumps(self.data)
 
+
 class AckMessage(MessageBase):
-    '''Simple message type meant for ACKnolegemnt of message receipt.'''
+
+    """Simple message type meant for ACKnolegemnt of message receipt."""
+
     def get_header(self, **kwargs):
         d = super(AckMessage, self).get_header(**kwargs)
         d['ACK'] = True
         d['ok'] = kwargs.get('ok', True)
         return d
+
 
 class ServerMessage(MessageBase):
     def __init__(self, **kwargs):
@@ -242,21 +272,19 @@ class ServerMessage(MessageBase):
                 if isinstance(self.data[0], complex):
                     is_complex = True
         self.data_is_complex = is_complex
+
     @classmethod
     def from_remote(cls, sock):
-        '''Reads data for the socket buffer and reconstructs the appropriate
+        """Reads data for the socket buffer and reconstructs the appropriate
         message that was sent by the other end.
 
         This method is used by clients to reconstruct ServerMessage objects
         and if necessary, use multiple read calls to get the entire message
         (if the message size is greater than the buffer length)
-        '''
+
+        """
         header = cls._recv(sock)
-        try:
-            kwargs = numpyjson.loads(header)
-        except ValueError:
-            #print(header)
-            raise
+        kwargs = numpyjson.loads(header)
         data_len = kwargs.get('data_len')
         data = kwargs.get('data')
         if data_len is None or data is not None:
@@ -273,15 +301,18 @@ class ServerMessage(MessageBase):
             data_len -= len(_recv)
         kwargs['data'] = numpyjson.loads(recv)
         return cls(**kwargs)
+
     def send_message(self, sock):
-        '''Sends the message data to clients.
+        """Sends the message data to clients.
+
         If necessary, uses multiple calls to send to ensure all data has
         actually been sent through the socket objects's buffer.
-        '''
+
+        """
         header, data = self._serialize()
 
         self._send(sock, header)
-        if data is not None and self.header.get('name') != 'read_samples_async':
+        if data is not None:
             ack = self.get_ack_response(sock)
             if not ack.header.get('ok'):
                 raise CommunicationError('No ACK received')
@@ -290,6 +321,7 @@ class ServerMessage(MessageBase):
                 sent = self._send(sock, data)
                 data_len -= sent
                 data = data[sent:]
+
     def get_header(self, **kwargs):
         d = super(ServerMessage, self).get_header(**kwargs)
         d['success'] = kwargs.get('success', True)
@@ -299,25 +331,31 @@ class ServerMessage(MessageBase):
         else:
             d['request'] = kwargs.get('request')
         return d
+
     def get_response_class(self):
         return AckMessage
+
 
 class ClientMessage(MessageBase):
     def send_message(self, sock):
         super(ClientMessage, self).send_message(sock)
         return self.get_response(sock)
+
     def get_header(self, **kwargs):
         d = super(ClientMessage, self).get_header(**kwargs)
         keys = ['type', 'name']
         d.update({k: kwargs.get(k) for k in keys})
         return d
+
     def get_response_class(self):
         return ServerMessage
+
 
 class RequestHandler(BaseRequestHandler):
     def setup(self):
         self.finished = False
         self.server.handlers.add(self)
+
     def handle(self, rx_message=None):
         if rx_message is None:
             rx_message = ClientMessage.from_remote(self.request)
@@ -333,10 +371,13 @@ class RequestHandler(BaseRequestHandler):
         if r is False:
             nak = AckMessage(ok=False)
             nak.send_message(self.request)
+
     def finish(self):
         self.server.handlers.discard(self)
+
     def close(self):
         self.finished = True
+
     def handle_method_call(self, rx_message):
         rtl_sdr = self.server.rtl_sdr
         method_name = rx_message.header.get('name')
@@ -346,13 +387,15 @@ class RequestHandler(BaseRequestHandler):
         try:
             m = getattr(rtl_sdr, method_name)
         except AttributeError:
-            raise CommunicationError('sdr has no attribute "%s"' % (method_name))
+            msg = 'sdr has no attribute "%s"' % (method_name)
+            raise CommunicationError(msg)
         if arg is not None:
             resp = m(arg)
         else:
             resp = m()
         tx_message = ServerMessage(client_message=rx_message, data=resp)
         tx_message.send_message(self.request)
+
     def handle_prop_set(self, rx_message):
         rtl_sdr = self.server.rtl_sdr
         prop_name = rx_message.header.get('name')
@@ -362,6 +405,7 @@ class RequestHandler(BaseRequestHandler):
         setattr(rtl_sdr, prop_name, value)
         tx_message = ServerMessage(client_message=rx_message)
         tx_message.send_message(self.request)
+
     def handle_prop_get(self, rx_message):
         prop_name = rx_message.header.get('name')
         if prop_name not in API_DESCRIPTORS:
@@ -371,24 +415,33 @@ class RequestHandler(BaseRequestHandler):
         tx_message = ServerMessage(client_message=rx_message, data=value)
         tx_message.send_message(self.request)
 
+
 class RtlSdrTcpClient(RtlSdrTcpBase):
-    '''Client object that connects to a remote server.
+
+    """Client object that connects to a remote server.
+
     Exposes most of the methods and descriptors that are available in the
     RtlSdr class in a transparent manner allowing an interface that is nearly
     identical to the core API.
-    '''
+
+    """
+
     def open(self, *args):
         self._socket = None
         self._keep_alive = False
         self.device_opened = True
+
     def close(self):
         self.device_opened = False
+
     def _build_socket(self):
         s = getattr(self, '_socket', None)
         if s is None:
-            s = self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s = self._socket = socket.socket(socket.AF_INET,
+                                             socket.SOCK_STREAM)
             s.connect((self.hostname, self.port))
         return s
+
     def _close_socket(self):
         if self._keep_alive:
             return
@@ -398,12 +451,14 @@ class RtlSdrTcpClient(RtlSdrTcpBase):
         print 'client closing socket'
         s.close()
         self._socket = None
+
     def _communicate(self, tx_message):
         s = self._build_socket()
         resp = tx_message.send_message(s)
         if isinstance(resp, ServerMessage):
             if not resp.header.get('success'):
-                raise CommunicationError('server was unsuccessful. msg=%s' % (tx_message.header))
+                msg = 'server was unsuccessful. msg=%s' % (tx_message.header)
+                raise CommunicationError(msg)
             resp_data = resp.data
         elif isinstance(resp, AckMessage):
             if not resp.header.get('ok'):
@@ -411,37 +466,52 @@ class RtlSdrTcpClient(RtlSdrTcpBase):
             resp_data = None
         self._close_socket()
         return resp_data
+
     def _communicate_method(self, method_name, arg=None):
         msg = ClientMessage(type='method', name=method_name, data=arg)
         return self._communicate(msg)
+
     def _communicate_descriptor_get(self, prop_name):
         msg = ClientMessage(type='prop_get', name=prop_name)
         return self._communicate(msg)
+
     def _communicate_descriptor_set(self, prop_name, value):
         msg = ClientMessage(type='prop_set', name=prop_name, data=value)
         return self._communicate(msg)
+
     def get_center_freq(self):
         return self._communicate_descriptor_get('fc')
+
     def set_center_freq(self, value):
         self._communicate_descriptor_set('fc', value)
+
     def get_sample_rate(self):
         return self._communicate_descriptor_get('rs')
+
     def set_sample_rate(self, value):
         self._communicate_descriptor_set('rs', value)
+
     def get_gain(self):
         return self._communicate_descriptor_get('gain')
+
     def set_gain(self, value):
         self._communicate_descriptor_set('gain', value)
+
     def get_freq_correction(self):
         return self._communicate_descriptor_get('freq_correction')
+
     def set_freq_correction(self, value):
         self._communicate_descriptor_set('freq_correction', value)
+
     def get_gains(self):
         return self._communicate_method('get_gains')
+
     def get_tuner_type(self):
         return self._communicate_method('get_tuner_type')
+
     def set_direct_sampling(self, value):
         self._communicate_method('set_direct_sampling', value)
+
     def read_samples(self, num_samples=RtlSdr.DEFAULT_READ_SIZE):
         return self._communicate_method('read_samples', num_samples)
     center_freq = fc = property(get_center_freq, set_center_freq)
@@ -449,10 +519,11 @@ class RtlSdrTcpClient(RtlSdrTcpBase):
     gain = property(get_gain, set_gain)
     freq_correction = property(get_freq_correction, set_freq_correction)
 
+
 def run_server():
-    '''Convenience function to run the server from the command line
+    """Convenience function to run the server from the command line
     with options for hostname, port and device index.
-    '''
+    """
     p = argparse.ArgumentParser()
     p.add_argument(
         '-a', '--address',
@@ -475,6 +546,7 @@ def run_server():
     o = vars(args)
     server = RtlSdrTcpServer(**o)
     server.run_forever()
+
 
 def test():
     import time
