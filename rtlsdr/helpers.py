cc1deaeeba2ecea3ee77688ca9afdb6708f3a05a
nocarryr <matt@nomadic-recording.com>
2015-08-31 13:59:57 -0500

    Remove NumpyJson implementation since it's no longer needed

--- rtlsdr/helpers.py
+++ rtlsdr/helpers.py
@@ -18,12 +18,6 @@
 from __future__ import division, print_function
 from functools import wraps
 import time
-import base64
-import json
-try:
-    import numpy as np
-except ImportError:
-    np = None
 
 def limit_time(max_seconds):
     '''Decorator to cancel async reads after "max_seconds" seconds elapse.
@@ -78,60 +72,6 @@ def limit_calls(max_calls):
 def test_callback(buffer, rtlsdr_obj):
     print('In callback')
     print('   signal mean:', sum(buffer)/len(buffer))
-
-## From http://stackoverflow.com/questions/27909658/
-
-
-class NumpyEncoder(json.JSONEncoder):
-    """JSONEncoder subclass to support serialization of numpy arrays.
-    Also handles complex numbers.
-    """
-    def default(self, obj):
-        if np is None:
-            if isinstance(obj, complex):
-                return dict(__complex__=[obj.real, obj.imag])
-        elif isinstance(obj, np.ndarray):
-            data_b64 = base64.b64encode(obj.dumps())
-            return dict(__ndarray__=data_b64)
-        return json.JSONEncoder.default(self, obj)
-
-
-def json_numpy_obj_hook(dct):
-    """Hook to deserialize numpy arrays and complex numbers formatted
-    by NumpyEncoder.
-    """
-    if isinstance(dct, dict):
-        if '__ndarray__' in dct:
-            data = base64.b64decode(dct['__ndarray__'])
-            return np.loads(data)
-        elif '__complex__' in dct:
-            data = dct['__complex__']
-            return complex(*data)
-    return dct
-
-
-class NumpyJson(object):
-    """Convenience class to emulate the builtin json module adding
-    a JSONEncoder and object hook to support numpy arrays and complex numbers.
-    """
-    def dumps(self, *args, **kwargs):
-        kwargs.setdefault('cls', NumpyEncoder)
-        return json.dumps(*args, **kwargs)
-
-    def loads(self, *args, **kwargs):
-        kwargs.setdefault('object_hook', json_numpy_obj_hook)
-        return json.loads(*args, **kwargs)
-
-    def dump(self, *args, **kwargs):
-        kwargs.setdefault('cls', NumpyEncoder)
-        return json.dump(*args, **kwargs)
-
-    def load(self, *args, **kwargs):
-        kwargs.setdefault('object_hook', json_numpy_obj_hook)
-        return json.load(*args, **kwargs)
-
-numpyjson = NumpyJson()
-##
 
 
 def main():
