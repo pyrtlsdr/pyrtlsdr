52ff074f2ba38f76057aa69b9de8ac046971e3dd
nocarryr <matt@nomadic-recording.com>
2016-02-15 17:14:08 -0600

    Remove scripts no longer needed

--- dumbtest.py
+++
@@ -1,7 +0,0 @@
-#   Temporary test just for travis-ci to see if things can even be imported
-#   Delete me when done
-
-import rtlsdr
-
-if __name__ == '__main__':
-    print('It works!')
--- testdummyrtlsdr.py
+++
@@ -1,7 +0,0 @@
-from rtlsdr.testutils import DummyRtlSdr
-from test import test
-
-
-if __name__ == '__main__':
-    sdr = DummyRtlSdr()
-    test(sdr)
