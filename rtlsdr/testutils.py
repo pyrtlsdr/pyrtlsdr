ee3c37bd1b5ef94e9f9cafade5692b1760a25d0c
nocarryr <matt@nomadic-recording.com>
2016-02-18 13:38:55 -0600

    PEP8

--- rtlsdr/testutils.py
+++ rtlsdr/testutils.py
@@ -9,8 +9,10 @@ except ImportError:
     from .rtlsdr import RtlSdr
     from .helpers import limit_calls
 
+
 def is_travisci():
-    return os.environ.get('CI') == 'true' and os.environ.get('TRAVIS') == 'true'
+    return all([os.environ.get(key) == 'true' for key in ['CI', 'TRAVIS']])
+
 
 class DummyRtlSdr(RtlSdr):
     """Subclass of :class:`rtlsdr.RtlSdr` to emulate a real device.
@@ -25,39 +27,55 @@ class DummyRtlSdr(RtlSdr):
         self._gain = 0.
         self._gains = list(range(0, 300, 25))
         super(DummyRtlSdr, self).__init__(device_index, test_mode_enabled)
+
     def open(self, device_index=0, test_mode_enabled=False):
         self.device_opened = True
         self.init_device_values()
+
     def close(self):
         if not self.device_opened:
             return
         self.device_opened = False
+
     def set_center_freq(self, freq):
         self._center_freq = freq
+
     def get_center_freq(self):
         return self._center_freq
+
     def set_freq_correction(self, err_ppm):
         self._freq_correction = err_ppm
+
     def get_freq_correction(self):
         return self._freq_correction
+
     def set_sample_rate(self, rate):
         self._sample_rate = rate
+
     def get_sample_rate(self):
         return self._sample_rate
+
     def set_gain(self, gain):
         self._gain = gain
+
     def get_gain(self):
         return self._gain
+
     def get_gains(self):
         return self._gains
+
     def set_manual_gain_enabled(self, enabled):
         pass
+
     def set_agc_mode(self, enabled):
         pass
+
     def set_direct_sampling(self, direct):
         pass
+
     def get_tuner_type(self):
         return 'dummy'
+
     def read_bytes(self, num_bytes=RtlSdr.DEFAULT_READ_SIZE):
         num_bytes = int(num_bytes)
         return [random.randint(0, 255) for i in range(num_bytes)]
@@ -66,7 +84,10 @@ class DummyRtlSdr(RtlSdr):
     gain = property(get_gain, set_gain)
     freq_correction = property(get_freq_correction, set_freq_correction)
 
-    def read_bytes_async(self, callback, num_bytes=RtlSdr.DEFAULT_READ_SIZE, context=None):
+    def read_bytes_async(self,
+                         callback,
+                         num_bytes=RtlSdr.DEFAULT_READ_SIZE,
+                         context=None):
         num_bytes = int(num_bytes)
         self._callback_bytes = callback
         if not context:
@@ -76,17 +97,26 @@ class DummyRtlSdr(RtlSdr):
         self.async_dummy.run()
         self.read_async_canceling = False
         self.async_dummy = None
+
     def _bytes_converter_callback(self, bytes_read):
         if self.read_async_canceling:
             return
         self._callback_bytes(bytes_read, self)
-    def read_samples_async(self, callback, num_samples=RtlSdr.DEFAULT_READ_SIZE, context=None):
+
+    def read_samples_async(self,
+                           callback,
+                           num_samples=RtlSdr.DEFAULT_READ_SIZE,
+                           context=None):
         num_bytes = 2*num_samples
         self._callback_samples = callback
-        self.read_bytes_async(self._samples_converter_callback, num_bytes, context)
+        self.read_bytes_async(self._samples_converter_callback,
+                              num_bytes,
+                              context)
+
     def cancel_read_async(self):
         self.read_async_canceling = True
         self.async_dummy.running = False
+
 
 class DummyAsyncSdr(object):
     """Simple object to emulate `rtlsdr_read_async` behavior.
@@ -101,6 +131,7 @@ class DummyAsyncSdr(object):
         # Guess how long it SHOULD take to read based off of the sample rate
         self.timeout = 1. / rtlsdr_obj.rs * num_samples
         self.running = False
+
     def run(self):
         sdr = self.rtlsdr_obj
         self.running = True
@@ -108,7 +139,6 @@ class DummyAsyncSdr(object):
             bytes_read = sdr.read_bytes(self.num_bytes)
             sdr._bytes_converter_callback(bytes_read)
             time.sleep(self.timeout)
-
 
 
 def check_close(num_digits, *args):
@@ -125,6 +155,7 @@ def check_close(num_digits, *args):
             return False
         last_n = n
     return True
+
 
 def build_test_sdr(sdr_cls, *args, **kwargs):
     """Functionality checks common to all tests
@@ -161,6 +192,7 @@ def build_test_sdr(sdr_cls, *args, **kwargs):
 
     return sdr
 
+
 def async_read_test(sdr, read_size=1024, num_callbacks=2, bytes_mode=False):
     @limit_calls(num_callbacks)
     def read_callback(data, rtlsdr_obj):
@@ -175,6 +207,7 @@ def async_read_test(sdr, read_size=1024, num_callbacks=2, bytes_mode=False):
         sdr.read_bytes_async(read_callback, read_size)
     else:
         sdr.read_samples_async(read_callback, read_size)
+
 
 def async_bytes_test(sdr, read_size=1024, num_callbacks=2):
     _async_read_test(sdr, read_size, num_callbacks, bytes_mode=True)
