d02dbf69e49e82f2de91845cedd45d9b7fe110d5
nocarryr <matt@nomadic-recording.com>
2016-11-10 15:14:30 -0600

    Add __version__ attribute to top-level module

--- rtlsdr/__init__.py
+++ rtlsdr/__init__.py
@@ -16,6 +16,12 @@
 
 import os
 import warnings
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.require('pyrtlsdr')[0].version
+except: # pragma: no cover
+    __version__ = 'unknown'
 
 RTLSDR_CLIENT_MODE = False
 if os.environ.get('RTLSDR_CLIENT_MODE', '').lower() in ['true', '1', 'yes']:
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,15 @@
 
+def test_pkg_version():
+    import subprocess
+    import rtlsdr
+
+    setup_version = subprocess.check_output(['python', 'setup.py', '-V'])
+    if isinstance(setup_version, bytes):
+        setup_version = setup_version.decode('UTF-8')
+    setup_version = setup_version.strip('\n')
+
+    assert rtlsdr.__version__ == setup_version
+
 def test(sdr_cls, use_numpy):
     from utils import generic_test
     sdr = sdr_cls()
