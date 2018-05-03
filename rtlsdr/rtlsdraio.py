cae2b66c22798c445b5c6f8fca0ed66102e00fee
nocarryr <matt@nomadic-recording.com>
2018-05-03 12:46:52 -0500

    Add more detail to the underlying operation of RtlSdrAio.stream method

--- rtlsdr/rtlsdraio.py
+++ rtlsdr/rtlsdraio.py
@@ -164,6 +164,15 @@ class RtlSdrAio(RtlSdr):
     def stream(self, num_samples_or_bytes=DEFAULT_READ_SIZE, format='samples', loop=None):
         """Start async streaming from SDR and return an async iterator (Python 3.5+).
 
+        The :meth:`read_samples_async` method is called in an  :class:`~concurrent.futures.Excecutor`
+        instance using :meth:`asyncio.AbstractEventLoop.run_in_executor`.
+
+        The returned asynchronous iterable can then used to retrieve sample
+        data using ``async for`` syntax.
+
+        Calling the :meth:`~rtlsdr.rtlsdraio.RtlSdrAio.stop` method will stop
+        the ``read_samples_async`` session and close the ``Excecutor`` task.
+
         Arguments:
             num_samples_or_bytes (int): The number of bytes/samples that will be
                 returned each iteration
@@ -171,8 +180,8 @@ class RtlSdrAio(RtlSdr):
                 or IQ samples ("samples") will be returned
             loop (optional): An asyncio event loop
 
-        Notes:
-            :meth:`~rtlsdr.rtlsdraio.RtlSdrAio.stop` should be called at some point.
+        Returns:
+            An ``asynchronous iterator`` to yield sample data
         """
         if format == 'samples':
             func_start = self.read_samples_async
@@ -190,5 +199,8 @@ class RtlSdrAio(RtlSdr):
 
     def stop(self):
         """Stop async stream
+
+        Stops the ``read_samples_async`` and ``Excecutor`` task created by
+        :meth:`stream`.
         """
         return asyncio.ensure_future(self.async_iter.stop(), loop=self.async_iter.loop)
