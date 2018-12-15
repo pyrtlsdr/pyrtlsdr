ebff9244d878a9360213ed403bd9bdd7dde2cd2d
nocarryr <matt@nomadic-recording.com>
2018-12-15 12:40:43 -0600

    Add Py3.6 and Py3.7 classifiers to setup metadata

--- setup.py
+++ setup.py
@@ -64,6 +64,8 @@ setup(
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  'Topic :: Utilities'],
     license='GPLv3',
     keywords='radio librtlsdr rtlsdr sdr',
