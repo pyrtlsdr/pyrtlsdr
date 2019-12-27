9d1acd8517ce48f6767738e7ce18a9f6710b709b
nocarryr <matt@nomadic-recording.com>
2019-12-27 09:22:33 -0600

    Use markers and collection hooks for '--skip-overrides' tests

--- tests/no_override_client_mode.py
+++ tests/no_override_client_mode.py
@@ -1,15 +1,11 @@
 import pytest
 
-no_overrides = pytest.mark.skipif(
-    not pytest.config.getoption('--no-overrides'),
-    reason='need --no-overrides to run'
-)
 
 @pytest.fixture
 def client_mode(monkeypatch):
     monkeypatch.setenv('RTLSDR_CLIENT_MODE', 'true')
 
-@no_overrides
+@pytest.mark.no_overrides
 def test_client_mode(client_mode):
     with pytest.warns(None) as record:
         import rtlsdr
