449bcb581f93a59a552cf7d06043e09db62d6a13
nocarryr <matt@nomadic-recording.com>
2019-12-27 10:39:58 -0600

    Remove sphinxcontrib-asyncio since Sphinx now supports async syntax

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,7 +38,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.asyncio',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
