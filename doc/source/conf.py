f0e1ec8176898790c5472e820d02e2541c6bca5d
nocarryr <matt@nomadic-recording.com>
2019-12-27 14:27:25 -0600

    Update dependencies and intersphinx mapping

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -308,6 +308,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python':('https://docs.python.org/3.5', None),
+    'python':('https://docs.python.org/', None),
     'numpy':('https://docs.scipy.org/doc/numpy/', None),
 }
